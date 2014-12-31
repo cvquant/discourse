747d3386d6d07b77e48d0a46d2656f3b5b14f47a
Nilesh Trivedi <nilesh.trivedi@tworoads.co.in>
2014-12-31 11:19:01 +0530

    Use domain: all, tld_length: 2 for session store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Discourse::Application.config.session_store :cookie_store, key: '_forum_session', domain: (Rails.env.development? ? '.qplum.dev' : '.qplum.co')
+Discourse::Application.config.session_store :cookie_store, key: '_forum_session', domain: :all, tld_length: 2
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
